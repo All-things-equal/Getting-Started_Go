427cdf720ff6d295833db14cd65596cffd368031
All-things-equal <all.things.equal@hotmail.com>
2022-10-19 16:48:07 +0800

    在包一层声明变量

--- Main.go
+++ Main.go
@@ -1,35 +1,24 @@
+/**
+ * @demo 在包一层声明变量
+ */
+
+/*
+ Tips:
+      * 无法用 varName := value 的方式声明变量
+*/
+
 package main
 
 import "fmt"
 
+var (
+	name   string = "name"
+	number int    = 3
+	season int    = 11
+)
+
 func main() {
-      fmt.Println("Hello, World!")
-      fmt.Println()
-      
-      /* *********************************
-       * Variables - 3种声明变量的方式
-      ********************************** */
-
-      var a int
-      a = 42
-      a = 27
-
-      var b float32 = 27
-
-      c := 27.0
-
-      fmt.Println(a, b, c)
-
-      fmt.Printf("%v, %T", a, a)
-      fmt.Println()
-      fmt.Printf("%v, %T", b, b)
-      fmt.Println()
-      fmt.Printf("%v, %T", c, c)
-      fmt.Println()
-
-      /* *********************************
-      // End of Variables ****************
-      ********************************** */
-
-      fmt.Print("\n\nEnd this program!\n")
+	fmt.Println(name)
+	fmt.Println(number)
+	fmt.Println(season)
 }
