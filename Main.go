9a7874ad23cfc99835cdab30a87d2b354808386d
All-things-equal <all.things.equal@hotmail.com>
2022-10-20 15:18:52 +0800

    变量类型转换

--- Main.go
+++ Main.go
@@ -1,27 +1,22 @@
 /**
- * @demo 变量命名规范及作用域
+ * @demo 变量类型转换
  */
-
-/*
- NotePad:
-      * 作用域 -- 3钟可见级别
-	  *. package level : 放在代码最顶部,用此可在整个package 中使用此变量
-	    *. 小写: 只能在包内部使用,但是在包内任何地方均可访问
-	    *. 大写: 暴露该变量(全局),可以被本包外的其他包访问,被调用
-	  *. funciton level : 可见范围在当前函数中
-	  *. block level : 代码块  在 {} 直接影响 {} 内的 变量
-*/
 
 package main
 
-import "fmt"
-
-var I int = 42
-
-var i int = 42
+import (
+	"fmt"
+	"strconv"
+)
 
 func main() {
-	var j int = 21
+	var i int = 42
+	fmt.Printf("%v, %T\n", i, i)
 
-	fmt.Println(i, j)
+	var j float32 = 0.0
+	j = float32(i)
+	fmt.Printf("%v, %T\n", j, j)
+
+	var s string = strconv.Itoa(i)
+	fmt.Printf("%v, %T\n", s, s)
 }
